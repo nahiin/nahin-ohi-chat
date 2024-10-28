38f29822458ef637a073ed27b72593ed0b2a5da8
Nahin <coradelaide@gmail.com>
2024-10-28 21:36:02 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,16 +5,15 @@ import { fileURLToPath } from 'url';
 const app = express();
 const port = process.env.PORT || 4000;
 
-// Get the directory name
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Serve static files from the React app's build directory
-app.use(express.static(path.join(__dirname, 'build')));
+// Serve static files from the React app's dist directory
+app.use(express.static(path.join(__dirname, '../dist')));
 
 // Handle any requests that don't match the ones above
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../dist', 'index.html'));
 });
 
 app.listen(port, () => {
